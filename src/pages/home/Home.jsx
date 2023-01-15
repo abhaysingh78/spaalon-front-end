2209d222cc09dfa7641c3c732dc15d00fc29b35e
princemahesh <princejagdishrohara@gmail.com>
2023-01-15 20:41:41 +0530

    footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Footer from "../../components/footer/Footer";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
