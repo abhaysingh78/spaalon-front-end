8d053d225acce6f86861e36947e53bfb7f126ec6
princemahesh <princejagdishrohara@gmail.com>
2023-01-16 12:27:18 +0530

    view card

--- src/components/viewallcard/ViewAll.jsx
+++ src/components/viewallcard/ViewAll.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const ViewAll = () => {
   return (
-    <div>
-      <div className=" bg-[url('https://images.pexels.com/photos/897262/pexels-photo-897262.jpeg?auto=compress&cs=tinysrgb&w=1000')] h-96 bg-no-repeat w-[100%] rounded-md flex my-12 items-center">
-        <div className="my-auto p-4 ml-8    ">
+    <div className="md:mx-48">
+      <div className="bg-cover   bg-[url('https://images.pexels.com/photos/897262/pexels-photo-897262.jpeg?auto=compress&cs=tinysrgb&w=1000')] h-96 bg-no-repeat w-[100%] rounded-md  flex my-12 items-center bg-opacity-">
+        <div className="my-auto p-4 ml-8  rounded  ">
           <span className="text-white ">SPAALON</span>
-          <h1 className="text-2xl text-white font-bold ">
+          <h1 className="text-3xl text-white font-bold mt-2">
             More than 3000 Spas & Salons
           </h1>
-          <p className="text-md text-white font-bold ">
+          <p className="text-xl text-white font-bold ">
             Book an appointment now within seconds
           </p>
-          <button className="bg-orange-500 rounded text-white my-2 py-1 px-4">
+          <button className="bg-orange-600 rounded font-semibold text-white mt-4 py-1 px-4">
             View All
           </button>
         </div>
