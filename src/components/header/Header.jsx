6e9b39abe817d3717adf6e229fa6ffb08c785af8
unknown <abhay112233chauhan@gmail.com>
2023-01-16 14:31:36 +0530

    header changed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,237 +1,151 @@
-import React from "react";
+import React, { useState } from "react";
+import "./header.css";
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+  // const showMenu = () => {
+  //   setShow(true);
+  //   console.log(show);
+  // };
+  const mobile_icon = document.getElementById("mobile-icon");
+  const mobile_menu = document.getElementById("mobile-menu");
+  const hamburger_icon = document.querySelector("#mobile-icon i");
+
+  function openCloseMenu() {
+    mobile_menu.classList.toggle("block");
+    mobile_menu.classList.toggle("active");
+  }
+
+  function changeIcon(icon) {
+    icon.classList.toggle("fa-xmark");
+  }
+
+  // mobile_icon.addEventListener('click', openCloseMenu);
   return (
-    <header class="relative flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
-      <nav
-        class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between"
-        aria-label="Global"
-      >
-        <div class="flex items-center justify-between">
-          <a class="flex-none text-xl font-semibold dark:text-white" href="#">
-            Brand
+    <header class='h-20 bg-white'>
+      <nav class='relative px-2 py-4'>
+        <div class='container mx-auto flex justify-between items-center'>
+          <img
+            src='https://avrasys.hu/logoipsum-logo-54.svg'
+            alt='Tailwindcss Navigation'
+          />
+
+          <ul class='hidden md:flex space-x-6'>
+            <li>
+              <a href='#'>Home</a>
+            </li>
+            <li>
+              <a href='#'>News</a>
+            </li>
+            <li class='flex relative group'>
+              <a href='#' class='mr-1'>
+                Services
+              </a>
+              <i class='fa-solid fa-chevron-down fa-2xs pt-3'></i>
+              {/* <!-- Submenu starts --> */}
+              <ul class='absolute bg-white p-3 w-52 top-6 transform scale-0 group-hover:scale-100 transition duration-150 ease-in-out origin-top shadow-lg'>
+                <li class='text-sm hover:bg-slate-100 leading-8'>
+                  <a href='#'>Webdesign</a>
+                </li>
+                <li class='text-sm hover:bg-slate-100 leading-8'>
+                  <a href='#'>Digital marketing</a>
+                </li>
+                <li class='text-sm hover:bg-slate-100 leading-8'>
+                  <a href='#'>SEO</a>
+                </li>
+                <li class='text-sm hover:bg-slate-100 leading-8'>
+                  <a href='#'>Ad campaigns</a>
+                </li>
+                <li class='text-sm hover:bg-slate-100 leading-8'>
+                  <a href='#'>UX Design</a>
+                </li>
+              </ul>
+              {/* <!-- Submenu ends --> */}
+            </li>
+            <li>
+              <a href='#'>About</a>
+            </li>
+            <li>
+              <a href='#'>Contact</a>
+            </li>
+          </ul>
+
+          <a
+            href='#'
+            class='bg-red-400 px-5 py-1 rounded-3xl hover:bg-red-500 text-white hidden md:flex'
+            role='button'
+          >
+            Sign In
           </a>
-          <div class="sm:hidden">
-            <button
-              type="button"
-              class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-gray-800 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
-              data-hs-collapse="#navbar-collapse-basic"
-              aria-controls="navbar-collapse-basic"
-              aria-label="Toggle navigation"
-            >
-              <svg
-                class="hs-collapse-open:hidden w-4 h-4"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-                />
-              </svg>
-              <svg
-                class="hs-collapse-open:block hidden w-4 h-4"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-              >
-                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-              </svg>
-            </button>
-          </div>
+
+          {/* <!-- Mobile menu icon --> */}
+          <button onClick={openCloseMenu} id='mobile-icon' class='md:hidden'>
+            <i onClick={() => changeIcon(this)} class='fa-solid fa-bars'>
+              121
+            </i>
+          </button>
         </div>
 
-        <div id="navbar-collapse-basic" class="hidden basis-full grow sm:block">
-          <div class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:pl-5">
-            <a class="font-medium text-blue-500" href="#" aria-current="page">
-              Active
-            </a>
+        {/* <!-- Mobile menu --> */}
+        <div class='md:hidden flex justify-center mt-3 w-full'>
+          <div id='mobile-menu' class='mobile-menu absolute top-23 w-full'>
+            <ul class='bg-gray-100 shadow-lg leading-9 font-bold h-screen'>
+              <li class='border-b-2 border-white hover:bg-red-400 hover:text-white pl-4'>
+                <a href='https://google.com' class='block pl-7'>
+                  Home
+                </a>
+              </li>
+              <li class='border-b-2 border-white hover:bg-red-400 hover:text-white pl-4'>
+                <a href='#' class='block pl-7'>
+                  News
+                </a>
+              </li>
+              <li class='border-b-2 border-white hover:bg-red-400 hover:text-white'>
+                <a href='#' class='block pl-11'>
+                  Services <i class='fa-solid fa-chevron-down fa-2xs pt-4'></i>
+                </a>
 
-            <div class="hs-dropdown [--strategy:static] sm:[--strategy:absolute] [--adaptive:none] sm:[--trigger:hover]">
-              <button
-                id="hs-mega-menu-basic-dr"
-                type="button"
-                class="flex items-center w-full text-gray-600 hover:text-gray-400 font-medium dark:text-gray-400 dark:hover:text-gray-500 "
-              >
-                Dropdown
-                <svg
-                  class="ml-2 w-2.5 h-2.5 text-gray-600"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                  ></path>
-                </svg>
-              </button>
-
-              <div class="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute top-full sm:border before:-top-5 before:left-0 before:w-full before:h-5">
-                <a
-                  class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                  href="#"
-                >
+                {/* <!-- Submenu starts --> */}
+                <ul class='bg-white text-gray-800 w-full'>
+                  <li class='text-sm leading-8 font-normal hover:bg-slate-200'>
+                    <a class='block pl-16' href='#'>
+                      Webdesign
+                    </a>
+                  </li>
+                  <li class='text-sm leading-8 font-normal hover:bg-slate-200'>
+                    <a class='block pl-16' href='#'>
+                      Digital marketing
+                    </a>
+                  </li>
+                  <li class='text-sm leading-8 font-normal hover:bg-slate-200'>
+                    <a class='block pl-16' href='#'>
+                      SEO
+                    </a>
+                  </li>
+                  <li class='text-sm leading-8 font-normal hover:bg-slate-200'>
+                    <a class='block pl-16' href='#'>
+                      Ad campaigns
+                    </a>
+                  </li>
+                  <li class='text-sm leading-8 font-normal hover:bg-slate-200'>
+                    <a class='block pl-16' href='#'>
+                      UX Design
+                    </a>
+                  </li>
+                </ul>
+                {/* <!-- Submenu ends --> */}
+              </li>
+              <li class='border-b-2 border-white hover:bg-red-400 hover:text-white pl-4'>
+                <a href='#' class='block pl-7'>
                   About
                 </a>
-                <div class="hs-dropdown relative [--strategy:static] sm:[--strategy:absolute] [--adaptive:none] sm:[--trigger:hover]">
-                  <button
-                    type="button"
-                    class="w-full flex justify-between w-full items-center text-sm text-gray-800 rounded-md py-2 px-3 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                  >
-                    Sub Menu
-                    <svg
-                      class="sm:-rotate-90 ml-2 w-2.5 h-2.5 text-gray-600"
-                      width="16"
-                      height="16"
-                      viewBox="0 0 16 16"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                      ></path>
-                    </svg>
-                  </button>
-
-                  <div class="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 sm:mt-2 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute sm:border before:-right-5 before:top-0 before:h-full before:w-5 top-0 right-full !mx-[10px]">
-                    <a
-                      class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      About
-                    </a>
-                    <a
-                      class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      Downloads
-                    </a>
-                    <a
-                      class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      Team Account
-                    </a>
-                  </div>
-                </div>
-
-                <a
-                  class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                  href="#"
-                >
-                  Downloads
+              </li>
+              <li class='border-b-2 border-white hover:bg-red-400 hover:text-white pl-4'>
+                <a href='#' class='block pl-7'>
+                  Contact
                 </a>
-                <a
-                  class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                  href="#"
-                >
-                  Team Account
-                </a>
-              </div>
-            </div>
-
-            <div class="hs-dropdown [--strategy:static] sm:[--strategy:absolute] [--adaptive:none] sm:[--trigger:hover]">
-              <button
-                type="button"
-                class="flex items-center w-full text-gray-600 hover:text-gray-400 font-medium dark:text-gray-400 dark:hover:text-gray-500"
-              >
-                Mega Menu
-                <svg
-                  class="ml-2 w-2.5 h-2.5 text-gray-600"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                  ></path>
-                </svg>
-              </button>
-
-              <div class="hs-dropdown-menu transition-[opacity,margin] sm:border duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 w-full hidden z-10 top-full left-0 min-w-[15rem] bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute before:-top-5 before:left-0 before:w-full before:h-5">
-                <div class="sm:grid sm:grid-cols-3">
-                  <div class="flex flex-col">
-                    <a
-                      class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      About
-                    </a>
-                    <a
-                      class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      Services
-                    </a>
-                    <a
-                      class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      Customer Story
-                    </a>
-                  </div>
-
-                  <div class="flex flex-col">
-                    <a
-                      class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      Careers
-                    </a>
-                    <a
-                      class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      Careers: Overview
-                    </a>
-                    <a
-                      class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      Careers: Apply
-                    </a>
-                  </div>
-
-                  <div class="flex flex-col">
-                    <a
-                      class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      Log In
-                    </a>
-                    <a
-                      class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      Sign Up
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <a
-              class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
-              href="#"
-            >
-              Link
-            </a>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
