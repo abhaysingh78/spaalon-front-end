ea68f82ac71e59b9ae80cba55268bc8412cc37d9
princemahesh <princejagdishrohara@gmail.com>
2023-01-16 20:50:01 +0530

    best deals and view all mobile fix

--- src/components/viewallcard/ViewAll.jsx
+++ src/components/viewallcard/ViewAll.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const ViewAll = () => {
   return (
-    <div className="md:mx-48 my-16 ">
-      <div className="bg-cover bg-gray-800   bg-[url('https://images.pexels.com/photos/897262/pexels-photo-897262.jpeg?auto=compress&cs=tinysrgb&w=1000')] h-96 bg-no-repeat w-[100%] rounded-md  flex my-12 items-center bg-opacity-">
-        <div className="my-auto p-4 ml-8  rounded  ">
+    <div className="mx-4 md:mx-48 my-16 ">
+      <div className=" relative bg-cover filter-brightness-[20%] bg-gray-800 before:absolute before:opacity-10  before:  bg-[url('https://images.pexels.com/photos/897262/pexels-photo-897262.jpeg?auto=compress&cs=tinysrgb&w=1000')] h-96 bg-no-repeat w-[100%] rounded-md  flex my-12 items-center bg-opacity-">
+        <div className="relative  my-auto p-4 ml-8  rounded  ">
           <span className="text-white ">SPAALON</span>
           <h1 className="text-3xl text-white font-bold mt-2">
             More than 3000 Spas & Salons
