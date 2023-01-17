10fa7dfe026eaeb5c8e614985c7d5e1d92703bd6
princemahesh <princejagdishrohara@gmail.com>
2023-01-17 18:31:17 +0530

    error fixed

--- src/components/poplular/Popular.jsx
+++ src/components/poplular/Popular.jsx
@@ -40,87 +40,87 @@ const Popular = ({ heading, dec, align, bg, style }) => {
     <div
       className={`xl:px-44 bg-${bg}-100 py-8 text-${align ? align : "center"}`}
     >
-      <div className='my-8 px-12' style={style && style}>
-        <hr className='my-6 w-20 border-2 border-orange-700 bg-orange-700' />
-        <h2 className='text-2xl font-semibold'>{heading}</h2>
-        <p className=' text-xl text-gray-700 font-semibold'>{dec}</p>
+      <div className="my-8 px-12" style={style && style}>
+        <hr className="my-6 w-20 border-2 border-orange-700 bg-orange-700" />
+        <h2 className="text-2xl font-semibold">{heading}</h2>
+        <p className=" text-xl text-gray-700 font-semibold">{dec}</p>
       </div>
       <Slider {...settings}>
-        <div class='flex px-4 flex-col my-2 drop-shadow-md outline-none rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
-          <div className='bg-white flex p-4 flex-col outline-none  shadow-sm dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7] rounded-lg'>
-            <div className='w-full flex items-center justify-center '>
-              <div className='flex flex-col items-center'>
+        <div class="flex px-4 flex-col my-2 drop-shadow-md outline-none rounded-xl">
+          <div className="bg-white flex p-4 flex-col outline-none  shadow-sm rounded-lg">
+            <div className="w-full flex items-center justify-center ">
+              <div className="flex flex-col items-center">
                 <img
-                  src='https://picsum.photos/800/600?random=1'
-                  alt=''
-                  className='hover:scale-105 transition-all ease-in-out'
+                  src="https://api.spaalon.com/media/shop_image/image_7.jpeg"
+                  alt=""
+                  className="hover:scale-105 transition-all ease-in-out"
                 />
-                <p className='mt-2 text-xs sm:text-sm md:text-base font-semibold text-left text-black'>
-                  Ricardo Boveta
+                <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-left text-black">
+                  Salon
                 </p>
               </div>
             </div>
           </div>
         </div>
-        <div class='flex px-4 flex-col my-2  drop-shadow-md outline-none rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
-          <div className='bg-white flex p-4 flex-col outline-none  shadow-sm dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7] rounded-lg'>
-            <div className='w-full flex items-center justify-center '>
-              <div className='flex flex-col items-center'>
+        <div class="flex px-4 flex-col my-2  drop-shadow-md outline-none rounded-xl ">
+          <div className="bg-white flex p-4 flex-col outline-none  shadow-sm rounded-lg">
+            <div className="w-full flex items-center justify-center ">
+              <div className="flex flex-col items-center">
                 <img
-                  src='https://picsum.photos/800/600?random=1'
-                  alt=''
-                  className='hover:scale-105 transition-all ease-in-out'
+                  src="https://api.spaalon.com/media/shop_image/image_7.jpeg"
+                  alt=""
+                  className="hover:scale-105 transition-all ease-in-out"
                 />
-                <p className='mt-2 text-xs sm:text-sm md:text-base font-semibold text-left text-black'>
-                  Ricardo Boveta
+                <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-left text-black">
+                  Salon
                 </p>
               </div>
             </div>
           </div>
         </div>
-        <div class='flex px-4 flex-col my-2  drop-shadow-md outline-none rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
-          <div className='bg-white flex p-4 flex-col outline-none  shadow-sm dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7] rounded-lg'>
-            <div className='w-full flex items-center justify-center '>
-              <div className='flex flex-col items-center'>
+        <div class="flex px-4 flex-col my-2  drop-shadow-md outline-none rounded-xl">
+          <div className="bg-white flex p-4 flex-col outline-none  shadow-sm rounded-lg">
+            <div className="w-full flex items-center justify-center ">
+              <div className="flex flex-col items-center">
                 <img
-                  src='https://picsum.photos/800/600?random=1'
-                  alt=''
-                  className='hover:scale-105 transition-all ease-in-out'
+                  src="https://api.spaalon.com/media/shop_image/image_7.jpeg"
+                  alt=""
+                  className="hover:scale-105 transition-all ease-in-out"
                 />
-                <p className='mt-2 text-xs sm:text-sm md:text-base font-semibold text-left text-black'>
-                  Ricardo Boveta
+                <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-left text-black">
+                  Salon
                 </p>
               </div>
             </div>
           </div>
         </div>
-        <div class='flex px-4 flex-col my-2  drop-shadow-md outline-none rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
-          <div className='bg-white flex p-4 flex-col outline-none  shadow-sm dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7] rounded-lg'>
-            <div className='w-full flex items-center justify-center '>
-              <div className='flex flex-col items-center'>
+        <div class="flex px-4 flex-col my-2  drop-shadow-md outline-none rounded-xl">
+          <div className="bg-white flex p-4 flex-col outline-none  shadow-sm rounded-lg">
+            <div className="w-full flex items-center justify-center ">
+              <div className="flex flex-col items-center">
                 <img
-                  src='https://picsum.photos/800/600?random=1'
-                  alt=''
-                  className='hover:scale-105 transition-all ease-in-out'
+                  src="https://api.spaalon.com/media/shop_image/image_7.jpeg"
+                  alt=""
+                  className="hover:scale-105 transition-all ease-in-out"
                 />
-                <p className='mt-2 text-xs sm:text-sm md:text-base font-semibold text-left text-black'>
-                  Ricardo Boveta
+                <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-left text-black">
+                  Salon
                 </p>
               </div>
             </div>
           </div>
         </div>
-        <div class='flex px-4 flex-col my-2  drop-shadow-md outline-none rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
-          <div className='bg-white flex p-4 flex-col outline-none  shadow-sm dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7] rounded-lg'>
-            <div className='w-full flex items-center justify-center '>
-              <div className='flex flex-col items-center'>
+        <div class="flex px-4 flex-col my-2  drop-shadow-md outline-none rounded-xl">
+          <div className="bg-white flex p-4 flex-col outline-none  shadow-sm rounded-lg">
+            <div className="w-full flex items-center justify-center ">
+              <div className="flex flex-col items-center">
                 <img
-                  src='https://picsum.photos/800/600?random=1'
-                  alt=''
-                  className='hover:scale-105 transition-all ease-in-out'
+                  src="https://api.spaalon.com/media/shop_image/image_7.jpeg"
+                  alt=""
+                  className="hover:scale-105 transition-all ease-in-out"
                 />
-                <p className='mt-2 text-xs sm:text-sm md:text-base font-semibold text-left text-black'>
-                  Ricardo Boveta
+                <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-left text-black">
+                  Salon
                 </p>
               </div>
             </div>
