34984378c88634d15491c59a3a65297fb357fd24
unknown <abhay112233chauhan@gmail.com>
2023-01-18 22:15:02 +0530

    fix carousel

--- src/components/poplular/Popular.jsx
+++ src/components/poplular/Popular.jsx
@@ -1,139 +1,74 @@
 import React, { useEffect, useState } from "react";
-import Slider from "react-slick";
 import { Link } from "react-router-dom";
-import axios from "axios";
+import Carousel from "react-multi-carousel";
 
 const Popular = ({ heading, dec, align, bg, style }) => {
   const [popular, setPopular] = useState([]);
 
   const fetchApi = async () => {
-    const res = await axios.get(
-      "https://spaalon.harij.in/api/shop/PopularShop"
-    );
-    // console.log(res.data.popularShop, "popular");
-    setPopular(res.data.popularShop);
+    let result = await fetch("https://spaalon.harij.in/api/shop/PopularShop");
+    result = await result.json();
+    setPopular(result.popularShop);
+    console.log(result.popularShop, "popular");
   };
 
   useEffect(() => {
     fetchApi();
   }, []);
 
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
   };
   return (
-    // {popular && popular?.map ()}
     <div
       className={`xl:px-44 bg-${bg}-100 py-8 text-${align ? align : "center"}`}
     >
-      <div className="my-8 px-12" style={style && style}>
-        <hr className="my-6 w-20 border-2 border-orange-700 bg-orange-700" />
-        <h2 className="text-2xl font-semibold">{heading}</h2>
-        <p className=" text-xl text-gray-700 font-semibold">{dec}</p>
+      <div className='my-8 px-12' style={style && style}>
+        <hr className='my-6 w-20 border-2 border-orange-700 bg-orange-700' />
+        <h2 className='text-2xl font-semibold'>{heading}</h2>
+        <p className=' text-xl text-gray-700 font-semibold'>{dec}</p>
       </div>
-      <Slider {...settings}>
-        <div class="flex px-4 flex-col my-2  outline-none rounded-xl ">
-          <div className="bg-white flex p-4 flex-col outline-none  rounded-lg">
-            <div className="w-full flex items-center justify-center ">
-              <Link to="/salon/details/1">
-                <div className="flex flex-col ">
-                  <img
-                    src="https://api.spaalon.com/media/shop_image/image_7.jpeg"
-                    alt=""
-                    className="hover:scale-105 transition-all ease-in-out"
-                  />
-                  <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-left text-black">
-                    Salon
-                  </p>
+      <Carousel responsive={responsive}>
+        {popular &&
+          popular?.map((item, i) => (
+            <div
+              key={i}
+              class='flex px-4 flex-col my-2 outline-none rounded-xl '
+            >
+              <div className='bg-white flex p-4 flex-col outline-none  rounded-lg'>
+                <div className='w-full flex items-center justify-center '>
+                  <Link to='/salon/details/1'>
+                    <div className='flex flex-col '>
+                      <img
+                        src={item.image_url}
+                        alt=''
+                        className='hover:scale-105 transition-all ease-in-out'
+                      />
+                      <p className='mt-2 text-xs sm:text-sm md:text-base font-semibold text-left text-black'>
+                        {item.shop_name}
+                      </p>
+                    </div>
+                  </Link>
                 </div>
-              </Link>
-            </div>
-          </div>
-        </div>
-
-        <div class="flex px-4 flex-col my-2   outline-none rounded-xl">
-          <div className="bg-white flex p-4 flex-col outline-none   rounded-lg">
-            <div className="w-full flex items-center justify-center ">
-              <Link>
-                <div className="flex flex-col ">
-                  <img
-                    src="https://api.spaalon.com/media/shop_image/image_7.jpeg"
-                    alt=""
-                    className="hover:scale-105 transition-all ease-in-out"
-                  />
-                  <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-left text-black">
-                    Salon
-                  </p>
-                  <p className="text-orange-600">Open</p>
-                </div>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div class="flex px-4 flex-col my-2   outline-none rounded-xl">
-          <div className="bg-white flex p-4 flex-col outline-none   rounded-lg">
-            <div className="w-full flex items-center justify-center ">
-              <div className="flex flex-col ">
-                <img
-                  src="https://api.spaalon.com/media/shop_image/image_7.jpeg"
-                  alt=""
-                  className="hover:scale-105 transition-all ease-in-out"
-                />
-                <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-left text-black">
-                  Salon
-                </p>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="flex px-4 flex-col my-2   outline-none rounded-xl">
-          <div className="bg-white flex p-4 flex-col outline-none   rounded-lg">
-            <div className="w-full flex items-center justify-center ">
-              <div className="flex flex-col ">
-                <img
-                  src="https://api.spaalon.com/media/shop_image/image_7.jpeg"
-                  alt=""
-                  className="hover:scale-105 transition-all ease-in-out"
-                />
-                <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-left text-black">
-                  Salon
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Slider>
+          ))}
+      </Carousel>
     </div>
   );
 };
--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -1,177 +1,77 @@
 import React from "react";
-import Slider from "react-slick";
 import { useState, useEffect } from "react";
-import axios from "axios";
+import Carousel from "react-multi-carousel";
 
 const Trending = ({ heading, dec, align, bg, style }) => {
   const [trending, setTrending] = useState([]);
 
   const fetchApi = async () => {
-    const res = await axios.get(
+    let result = await fetch(
       "https://spaalon.harij.in/api/shop/TrendingService"
     );
-
-    setTrending(res.data.trendingservice);
-    console.log(res.data.trendingservice, "trending");
+    result = await result.json();
+    setTrending(result.trendingservice);
   };
 
   useEffect(() => {
     fetchApi();
   }, []);
 
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
   };
+
   return (
     <div
       className={`xl:px-44 bg-${bg}-100 py-8 text-${align ? align : "center"}`}
     >
-      <div className="my-8 px-12" style={style && style}>
-        <hr className="my-6 w-20 border-2 border-orange-700 bg-orange-700" />
-        <h2 className="text-2xl font-semibold">{heading}</h2>
-        <p className=" text-xl text-gray-700 font-semibold">{dec}</p>
+      <div className='my-8 px-12' style={style && style}>
+        <hr className='my-6 w-20 border-2 border-orange-700 bg-orange-700' />
+        <h2 className='text-2xl font-semibold'>{heading}</h2>
+        <p className=' text-xl text-gray-700 font-semibold'>{dec}</p>
       </div>
 
-      <Slider {...settings}>
-        <div className="flex px-4 flex-col outline-none rounded-xl ">
-          <div className="bg-white flex p-4 flex-col outline-none   shadow-sm   rounded-lg">
-            <div className="w-full flex items-center justify-center ">
-              <div className="flex flex-col items-center rounded-full">
-                <img
-                  src="https://spaalon.com/images/serviceImg.png"
-                  alt=""
-                  style={{
-                    width: "170px",
-                    height: "170px",
-                    objectFit: "contain",
-                  }}
-                  className="rounded-full"
-                />
+      <Carousel responsive={responsive}>
+        {trending?.map((item, i) => (
+          <div className='flex px-4 flex-col outline-none rounded-xl '>
+            <div className='bg-white flex p-4 flex-col outline-none shadow-sm rounded-lg'>
+              <div className='w-full flex items-center justify-center '>
+                <div className='flex flex-col items-center rounded-full'>
+                  <img
+                    src={item.image_url}
+                    alt=''
+                    style={{
+                      width: "170px",
+                      height: "170px",
+                      objectFit: "contain",
+                    }}
+                    className='rounded-full'
+                  />
 
-                <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-center text-black">
-                  MakeUp33
-                </p>
+                  <p className='mt-2 text-xs sm:text-sm md:text-base font-semibold text-center text-black'>
+                    {item.name}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        
-        <div class="flex px-4 flex-col outline-none rounded-xl  ">
-          <div className="bg-white flex p-4 flex-col outline-none  shadow-sm  rounded-lg">
-            <div className="w-full flex items-center justify-center ">
-              <div className="flex flex-col items-center">
-                <img
-                  src="https://spaalon.com/images/serviceImg.png"
-                  alt=""
-                  className="rounded-full w-28"
-                  style={{
-                    width: "170px",
-                    height: "170px",
-                    objectFit: "contain",
-                  }}
-                />
-                <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-center text-black">
-                  Hair Cut
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>{" "}
-        <div class="flex px-4 flex-col outline-none rounded-xl ">
-          <div className="bg-white flex p-4 flex-col outline-none  shadow-sm  rounded-lg">
-            <div className="w-full flex items-center justify-center ">
-              <div className="flex flex-col items-center">
-                <img
-                  src="https://spaalon.com/images/serviceImg.png"
-                  alt=""
-                  className="rounded-full w-28"
-                  style={{
-                    width: "170px",
-                    height: "170px",
-                    objectFit: "contain",
-                  }}
-                />
-                <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-center text-black">
-                  Hair Cut
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>{" "}
-        <div class="flex px-4 flex-col outline-none rounded-xl ">
-          <div className="bg-white flex p-4 flex-col outline-none  shadow-sm   rounded-lg">
-            <div className="w-full flex items-center justify-center ">
-              <div className="flex flex-col items-center">
-                <img
-                  src="https://spaalon.com/images/serviceImg.png"
-                  alt=""
-                  className="rounded-full w-28"
-                  style={{
-                    width: "170px",
-                    height: "170px",
-                    objectFit: "contain",
-                  }}
-                />
-                <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-center text-black">
-                  Hair Cut
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>{" "}
-        <div class="flex px-4 flex-col  outline-none rounded-xl ">
-          <div className="bg-white flex p-4 flex-col outline-none  shadow-sm  rounded-lg">
-            <div className="w-full flex items-center justify-center ">
-              <div className="flex flex-col items-center">
-                <img
-                  src="https://spaalon.com/images/serviceImg.png"
-                  alt=""
-                  className="rounded-full w-28"
-                  style={{
-                    width: "170px",
-                    height: "170px",
-                    objectFit: "contain",
-                  }}
-                />
-                <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-center text-black">
-                  Hair Cut
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Slider>
+        ))}
+      </Carousel>
     </div>
   );
 };
