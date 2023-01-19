8a0a5c71c5fc6648d2334f8d650c5802b4a2f68b
princemahesh <princejagdishrohara@gmail.com>
2023-01-19 09:12:19 +0530

    some issues fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import "react-multi-carousel/lib/styles.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
+// import "react-multi-carousel/lib/styles.css";
 import App from "./App";
 import "./index.css";
 
